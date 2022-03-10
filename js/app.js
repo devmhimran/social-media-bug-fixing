6fef7ff7482c9db065f964f30c573b78328afa63
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-03-10 23:50:31 +0600

    Fix Liked Post Repeat

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
+        document.getElementById( "liked" ).textContent = "";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
 
